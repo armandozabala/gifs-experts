14bf577820e694719944b5622a48000a5c3f6642
armandozabala <armandozabala@gmail.com>
2021-05-11 22:07:51 -0500

    first commit docs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -11,14 +11,13 @@ const GifGrid = ({ category }) => {
 
     return (
         <>
-        <h3>{category}</h3>
+        <h3> - {category} -</h3>
 
         { loading && <p className="animate__animated animate__flash">Loading </p>}
 
      
         <div className="card-grid">
           
-         
                 {
                     data.map(img => (
                          <GifGridItem 
